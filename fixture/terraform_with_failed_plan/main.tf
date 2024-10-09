c7c06ddb882867aee6850f39ab0f0a56c2764667
Glenn Terjesen <glenn.terjesen@gmail.com>
2024-10-09 13:53:45 +0200

    fix: test output

--- fixture/terraform_with_failed_plan/main.tf
+++ fixture/terraform_with_failed_plan/main.tf
@@ -1,7 +1,7 @@
-# Terraform Entur init configuration
+#   Terraform Entur init configuration
 module "init" {
   source      = "github.com/entur/terraform-google-init//modules/init?ref=v1.0.0"
-  app_id      = "ghaci"
+   app_id      = "ghaci"
   environment = var.environment
 }
 resource "null_resource" "dummy" {
