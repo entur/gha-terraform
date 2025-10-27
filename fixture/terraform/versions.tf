7a58782910d9cb73a152b52b88c316cef28773d7
Glenn Terjesen <glenn.terjesen@gmail.com>
2025-10-27 09:50:40 +0100

    fix: required providers version

--- fixture/terraform/versions.tf
+++ fixture/terraform/versions.tf
@@ -1,4 +1,10 @@
 terraform {
   # Leave this version be, to test terraform version fetching
   required_version = "1.8.3"
+  required_providers {
+    null = {
+      source  = "hashicorp/null"
+      version = "3.2.4"
+    }
+  }
 }
